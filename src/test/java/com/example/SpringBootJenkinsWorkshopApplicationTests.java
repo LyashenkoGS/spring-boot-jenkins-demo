44c93fc1225131edc52d3332a6ef9cbc447f1599
lyashenkogs <lyashenkogs@gmail.com>
2017-12-12 01:26:55 +0200

    Broke

--- src/test/java/com/example/SpringBootJenkinsWorkshopApplicationTests.java
+++ src/test/java/com/example/SpringBootJenkinsWorkshopApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,8 @@ public class SpringBootJenkinsWorkshopApplicationTests {
 
     @Test
     public void contextLoads() {
+		Assert.assertEquals(0,1);
+		//bbbbbb
     }
 
 }
