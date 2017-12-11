4ace7db51e8122d4b0be2a940a4f88fcb03c1503
lyashenkogs <lyashenkogs@gmail.com>
2017-12-11 14:32:17 +0200

    Broke a build

--- src/test/java/com/example/SpringBootJenkinsWorkshopApplicationTests.java
+++ src/test/java/com/example/SpringBootJenkinsWorkshopApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ public class SpringBootJenkinsWorkshopApplicationTests {
 
     @Test
     public void contextLoads() {
+		Assert.assertEquals(0,1);
     }
 
 }
